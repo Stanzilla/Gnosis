6f53ec95050332ff5b8bd2fb5cb3c6da7e2dd9e1
elchefe <akreidl@gmail.com>
2013-01-03 18:30:38 +0100

    added new textures and font

--- Variables.lua
+++ Variables.lua
@@ -97,7 +97,7 @@ Gnosis.colClasses = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 3.27;
+	self.ver = 3.99;
 	self.optver = 3.25;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
