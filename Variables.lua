5944021c0d25e57cf5623e09c8f4f1ffb076bf7e
elchefe <none@none>
2015-01-08 23:53:12 +0100

    changed counter example (gnosis_howto_timers.pdf) version bump

--- Variables.lua
+++ Variables.lua
@@ -144,7 +144,7 @@ Gnosis.tSoundChannels = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 4.41;
+	self.ver = 4.42;
 	self.optver = 3.25;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
