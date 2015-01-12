160cd666885649802b8dc7e01d7ccc591060e024
elchefe <none@none>
2015-01-12 04:10:46 +0100

    fixed problem with some (Insanity?) spells not triggering the global cooldown

--- Bars.lua
+++ Bars.lua
@@ -375,7 +375,8 @@ function Gnosis:FindCBNext(unit)
 end
 
 function Gnosis:FindGCDBars(spell, rank, fCurTime)
-	local start, cd = GetSpellCooldown(spell);
+	-- using "Global Cooldown" spell
+	local start, cd = GetSpellCooldown(61304);
 	if (not start or not(cd > 0 and cd <= 1.5)) then
 		return;
 	end
