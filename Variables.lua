04dbbe2eab1392b2846633e1a4a09344c61bf515
elchefe <none@none>
2017-04-03 22:29:13 +0200

    version update to 4.64

--- Variables.lua
+++ Variables.lua
@@ -141,7 +141,7 @@ Gnosis.tSoundChannels = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 4.63;
+	self.ver = 4.64;
 	self.optver = 4.62;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
