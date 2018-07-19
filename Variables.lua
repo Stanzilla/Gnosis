f9e6f5e796c517b2b9e01c2d55a1e7aca6a5639e
Elvenbane <elvenbane@clanyawa.com>
2018-07-19 08:33:09 -0500

    Merge from https://github.com/voidzone/Gnosis/tree/BFA Version increment

--- Variables.lua
+++ Variables.lua
@@ -141,7 +141,7 @@ Gnosis.tSoundChannels = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 4.65;
+	self.ver = 4.66;
 	self.optver = 4.62;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
