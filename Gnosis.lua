e59112876c7e3878ab7db381442cc2a8ababc95a
elchefe12345 <none@none>
2020-10-25 01:23:49 +0200

    pulling in latest revisions of LibClassicDurations and LibClassicCasterino instead of latest tag; delayed bandaid for LibDialog-1.0 to make sure latest version from any addon has been loaded

--- Gnosis.lua
+++ Gnosis.lua
@@ -53,20 +53,6 @@ if (wowclassic and Gnosis.libclcno) then
 	end
 end
 
--- bandaid for LibDialog-1.0 (MINOR <= 8, until it is fixed)
-if (wowmainline) then
-	_, libdialog_ver = LibStub:GetLibrary("LibDialog-1.0");
-	
-	if (libdialog_ver and libdialog_ver <= 8) then
-		hooksecurefunc(_G, 'CreateFrame', function(frameType, frameName)
-				if (frameName and string_find(frameName, "LibDialog%-1%.0_Dialog") and _G[frameName]) then
-					_G[frameName] = Mixin(_G[frameName], BackdropTemplateMixin);
-				end
-			end
-		);
-	end
-end
-
 -- LibSharedMedia
 if(Gnosis.lsm) then
 	-- statusbar textures
@@ -435,6 +421,18 @@ function Gnosis:SetupHooks()
 				Gnosis:CloseAllTradeskillBars();
 			end
 		);
+		
+		-- bandaid for LibDialog-1.0 (MINOR <= 8, until it is fixed)
+		local ld_name, ld_ver = LibStub:GetLibrary("LibDialog-1.0");
+	
+		if (ld_ver and ld_ver <= 8) then
+			hooksecurefunc(_G, 'CreateFrame', function(frameType, frameName)
+					if (frameName and string_find(frameName, "LibDialog%-1%.0_Dialog") and _G[frameName]) then
+						_G[frameName] = Mixin(_G[frameName], BackdropTemplateMixin);
+					end
+				end
+			);
+		end
 	elseif (wowclassic) then
 		-- tradeskill hooking
 		hooksecurefunc('DoTradeSkill', function(index, num)
--- Variables.lua
+++ Variables.lua
@@ -174,7 +174,7 @@ Gnosis.tSoundChannels = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 4.78;
+	self.ver = 4.79;
 	self.optver = 4.62;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
