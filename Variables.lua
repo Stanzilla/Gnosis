c8a895cebae8bf417f18fa56e8f76097842f82a7
Elvenbane <elvenbane@clanyawa.com>
2022-11-14 18:20:16 -0600

    Update Variables.lua
    4.94

--- Variables.lua
+++ Variables.lua
@@ -194,7 +194,7 @@ Gnosis.tSoundChannels = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 4.93;
+	self.ver = 4.94;
 	self.optver = 4.62;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
